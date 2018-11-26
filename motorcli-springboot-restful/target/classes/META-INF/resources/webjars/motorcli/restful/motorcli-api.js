89abc579884c17fa9a06dfc11692bdd0b81d906b
Li Yuan Jun <throwexc@sina.com>
2018-11-26 15:50:22 +0800

    Motor CLI Application Api Docs 优化

--- motorcli-springboot-restful/target/classes/META-INF/resources/webjars/motorcli/restful/motorcli-api.js
+++ motorcli-springboot-restful/target/classes/META-INF/resources/webjars/motorcli/restful/motorcli-api.js
@@ -109,27 +109,46 @@ var mcApi = mcApi || {};
       mcApi.baseUrl = url;
     };
 
+    mcApi.resetAuthStatus = function() {
+        if (mcApi.auth.getToken()) {
+
+            $("button.unlocked").removeClass("unlocked").addClass("locked").html("<svg width=\"20\" height=\"20\"><use href=\"#locked\" xlink:href=\"#locked\"><svg viewBox=\"0 0 20 20\" id=\"locked\" width=\"100%\" height=\"100%\">\n" +
+                "      <path d=\"M15.8 8H14V5.6C14 2.703 12.665 1 10 1 7.334 1 6 2.703 6 5.6V8H4c-.553 0-1 .646-1 1.199V17c0 .549.428 1.139.951 1.307l1.197.387C5.672 18.861 6.55 19 7.1 19h5.8c.549 0 1.428-.139 1.951-.307l1.196-.387c.524-.167.953-.757.953-1.306V9.199C17 8.646 16.352 8 15.8 8zM12 8H8V5.199C8 3.754 8.797 3 10 3c1.203 0 2 .754 2 2.199V8z\"></path>\n" +
+                "    </svg></use></svg>");
+
+        } else {
+            $("button.locked").removeClass("locked").addClass("unlocked").html("<svg width=\"20\" height=\"20\"><use href=\"#unlocked\" xlink:href=\"#unlocked\"><svg viewBox=\"0 0 20 20\" id=\"unlocked\" width=\"100%\" height=\"100%\">\n" +
+                "      <path d=\"M15.8 8H14V5.6C14 2.703 12.665 1 10 1 7.334 1 6 2.703 6 5.6V6h2v-.801C8 3.754 8.797 3 10 3c1.203 0 2 .754 2 2.199V8H4c-.553 0-1 .646-1 1.199V17c0 .549.428 1.139.951 1.307l1.197.387C5.672 18.861 6.55 19 7.1 19h5.8c.549 0 1.428-.139 1.951-.307l1.196-.387c.524-.167.953-.757.953-1.306V9.199C17 8.646 16.352 8 15.8 8z\"></path>\n" +
+                "    </svg></use></svg>");
+        }
+    };
+
+    mcApi.onComplete = function() {
+        var $btn = $("#authorize");
+
+        if (mcApi.auth.getToken()) {
+            $btn.html("Logout<i class=\"fa fa-lock\" style=\"margin-left: 20px;\"></i>");
+            $btn.addClass("lock");
+        } else {
+            $btn.html("Authorize<i class=\"fa fa-unlock-alt\" style=\"margin-left: 20px;\"></i>");
+            $btn.removeClass("lock");
+        }
+
+        setTimeout(function() {
+            $(".swagger-ui .wrapper .opblock-tag-section").click(function() {
+                setTimeout(function() {
+                    mcApi.resetAuthStatus();
+                }, 500);
+            });
+        }, 1000);
+
+        mcApi.resetAuthStatus();
+    };
+
     mcApi.onApiChange = function() {
-        mcApi.ready(function() {
-            var $btn = $("#authorize");
-
-            if (mcApi.auth.getToken()) {
-                $btn.html("Logout<i class=\"fa fa-lock\" style=\"margin-left: 20px;\"></i>");
-                $btn.addClass("lock");
-
-                $("button.unlocked").removeClass("unlocked").addClass("locked").html("<svg width=\"20\" height=\"20\"><use href=\"#locked\" xlink:href=\"#locked\"><svg viewBox=\"0 0 20 20\" id=\"locked\" width=\"100%\" height=\"100%\">\n" +
-                    "      <path d=\"M15.8 8H14V5.6C14 2.703 12.665 1 10 1 7.334 1 6 2.703 6 5.6V8H4c-.553 0-1 .646-1 1.199V17c0 .549.428 1.139.951 1.307l1.197.387C5.672 18.861 6.55 19 7.1 19h5.8c.549 0 1.428-.139 1.951-.307l1.196-.387c.524-.167.953-.757.953-1.306V9.199C17 8.646 16.352 8 15.8 8zM12 8H8V5.199C8 3.754 8.797 3 10 3c1.203 0 2 .754 2 2.199V8z\"></path>\n" +
-                    "    </svg></use></svg>");
-
-            } else {
-                $btn.html("Authorize<i class=\"fa fa-unlock-alt\" style=\"margin-left: 20px;\"></i>");
-                $btn.removeClass("lock");
-
-                $("button.locked").removeClass("locked").addClass("unlocked").html("<svg width=\"20\" height=\"20\"><use href=\"#unlocked\" xlink:href=\"#unlocked\"><svg viewBox=\"0 0 20 20\" id=\"unlocked\" width=\"100%\" height=\"100%\">\n" +
-                    "      <path d=\"M15.8 8H14V5.6C14 2.703 12.665 1 10 1 7.334 1 6 2.703 6 5.6V6h2v-.801C8 3.754 8.797 3 10 3c1.203 0 2 .754 2 2.199V8H4c-.553 0-1 .646-1 1.199V17c0 .549.428 1.139.951 1.307l1.197.387C5.672 18.861 6.55 19 7.1 19h5.8c.549 0 1.428-.139 1.951-.307l1.196-.387c.524-.167.953-.757.953-1.306V9.199C17 8.646 16.352 8 15.8 8z\"></path>\n" +
-                    "    </svg></use></svg>");
-            }
-        });
+        // mcApi.ready(function() {
+        //
+        // });
     };
 
     mcApi.authorizeBtnClick = function() {
@@ -139,19 +158,13 @@ var mcApi = mcApi || {};
             mcApi.swagger.logout();
             $btn.html("Authorize<i class=\"fa fa-unlock-alt\" style=\"margin-left: 20px;\"></i>");
             $btn.removeClass("lock");
-
-            $("button.locked").removeClass("locked").addClass("unlocked").html("<svg width=\"20\" height=\"20\"><use href=\"#unlocked\" xlink:href=\"#unlocked\"><svg viewBox=\"0 0 20 20\" id=\"unlocked\" width=\"100%\" height=\"100%\">\n" +
-                "      <path d=\"M15.8 8H14V5.6C14 2.703 12.665 1 10 1 7.334 1 6 2.703 6 5.6V6h2v-.801C8 3.754 8.797 3 10 3c1.203 0 2 .754 2 2.199V8H4c-.553 0-1 .646-1 1.199V17c0 .549.428 1.139.951 1.307l1.197.387C5.672 18.861 6.55 19 7.1 19h5.8c.549 0 1.428-.139 1.951-.307l1.196-.387c.524-.167.953-.757.953-1.306V9.199C17 8.646 16.352 8 15.8 8z\"></path>\n" +
-                "    </svg></use></svg>");
+            mcApi.resetAuthStatus();
         } else {
             mcApi.swagger.openAuthDialog(function() {
                 $btn.html("Logout<i class=\"fa fa-lock\" style=\"margin-left: 20px;\"></i>");
                 $btn.addClass("lock");
                 mcApi.swagger.closeAuthDialog();
-
-                $("button.unlocked").removeClass("unlocked").addClass("locked").html("<svg width=\"20\" height=\"20\"><use href=\"#locked\" xlink:href=\"#locked\"><svg viewBox=\"0 0 20 20\" id=\"locked\" width=\"100%\" height=\"100%\">\n" +
-                    "      <path d=\"M15.8 8H14V5.6C14 2.703 12.665 1 10 1 7.334 1 6 2.703 6 5.6V8H4c-.553 0-1 .646-1 1.199V17c0 .549.428 1.139.951 1.307l1.197.387C5.672 18.861 6.55 19 7.1 19h5.8c.549 0 1.428-.139 1.951-.307l1.196-.387c.524-.167.953-.757.953-1.306V9.199C17 8.646 16.352 8 15.8 8zM12 8H8V5.199C8 3.754 8.797 3 10 3c1.203 0 2 .754 2 2.199V8z\"></path>\n" +
-                    "    </svg></use></svg>");
+                mcApi.resetAuthStatus();
             });
         }
     };
